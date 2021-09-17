9f7bfe4f2083b8ccc6c345d1d98903732d367610
a <1179938183@qq.com>
2021-09-17 23:25:16 +0800

    增加单元测试代码

--- 个人项目/文本查重程序/src/com/test/testCase.java
+++ 个人项目/文本查重程序/src/com/test/testCase.java
@@ -23,19 +23,18 @@ public class testCase {
     public void testWordsUtil() {
         // 测试正常情况下对文本进行分词
         List<Word> cutFileContent1 = WordsUtil.cutFileContent("今天是星期天,天气晴,今天晚上我要去看电影。");
+        System.out.println("正常情况下: " + cutFileContent1);
         // 测试字符串倒序情况下对文本进行分词
         List<Word> cutFileContent2 = WordsUtil.cutFileContent("影电看去要我上晚天今,晴气天,天期星是天今。");
+        System.out.println("字符串倒序情况下: " + cutFileContent2);
         // 测试字符串中加入乱码情况下对文本进行分词
         List<Word> cutFileContent3 = WordsUtil.cutFileContent("今!#%天是星期&&天,天气晴,今天晚**上我要去(看电@影。");
+        System.out.println("字符串中加入乱码情况下: " + cutFileContent3);
         // 测试字符串中有空格情况下对文本进行分词
         List<Word> cutFileContent4 = WordsUtil.cutFileContent("今天是星期  天,天  气晴,今天晚  上我要去看电  影。");
+        System.out.println("字符串中有空格情况下" + cutFileContent4);
         // 测试空串情况下对文本进行分词
         List<Word> cutFileContent5 = WordsUtil.cutFileContent("");
-        // 结果输出
-        System.out.println("正常情况下: " + cutFileContent1);
-        System.out.println("字符串倒序情况下: " + cutFileContent2);
-        System.out.println("字符串中加入乱码情况下: " + cutFileContent3);
-        System.out.println("字符串中有空格情况下" + cutFileContent4);
         System.out.println("空串情况下: " + cutFileContent5);
     }
 
@@ -63,15 +62,15 @@ public class testCase {
         duplicateCheckingRate = Tf_IdfUtil.similarity(sourceCutFileContent, copyCutFileContent);
         System.out.println("字符串存在部分改动情况: " + duplicateCheckingRate);
 
-        // 测试字符串为空串的情况
+        // 测试字符串部分缺失情况
         copyCutFileContent = WordsUtil.cutFileContent("今天是星期,天晴,今天晚我要去电影。");
         duplicateCheckingRate = Tf_IdfUtil.similarity(sourceCutFileContent, copyCutFileContent);
-        System.out.println("字符串为空串情况: " + duplicateCheckingRate);
+        System.out.println("字符串部分缺失情况: " + duplicateCheckingRate);
 
-        // 测试字符串部分缺失情况
+        // 测试字符串为空串的情况
         sourceCutFileContent = WordsUtil.cutFileContent("");
         copyCutFileContent = WordsUtil.cutFileContent("");
         duplicateCheckingRate = Tf_IdfUtil.similarity(sourceCutFileContent, copyCutFileContent);
-        System.out.println("字符串部分缺失情况: " + duplicateCheckingRate);
+        System.out.println("字符串为空串情况: " + duplicateCheckingRate);
     }
 }
