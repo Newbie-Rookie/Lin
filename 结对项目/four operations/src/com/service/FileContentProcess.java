814b588c80e596a1f593420ff9e8151fbfabbf07
a <1179938183@qq.com>
2021-10-22 15:51:00 +0800

    文件处理类——读写文件方法

---
+++ 结对项目/four operations/src/com/service/FileContentProcess.java
@@ -0,0 +1,74 @@
+package com.service;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ *  读写文件类
+ */
+public class FileContentProcess {
+    /**
+     * 读文件方法
+     * @param filePath
+     * @return
+     */
+    public static String read(String filePath){
+        FileReader fr = null;
+        // 存储读取文件的内容
+        StringBuffer fileContent = new StringBuffer();
+        try {
+            // 创建字符输入流
+            fr = new FileReader(filePath);
+            // 创建字符数组进行读存储
+            char[] chars = new char[512];
+            // 统计每次读取字符数,读到文件末尾为-1
+            int readChars;
+            // 读文件
+            while((readChars = fr.read(chars)) != -1){
+                fileContent.append(new String(chars,0,readChars));
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if(fr != null){
+                try {
+                    fr.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return fileContent.toString();
+    }
+
+    /**
+     * 写文件方法
+     * @param filePath
+     * @param fileContent
+     */
+    public static void write(String filePath,String fileContent){
+        FileWriter fw = null;
+        try {
+            // 创建字符输出流对象
+            fw = new FileWriter(filePath);
+            // 写文件
+            fw.write(fileContent);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if(fw != null){
+                try {
+                    fw.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
