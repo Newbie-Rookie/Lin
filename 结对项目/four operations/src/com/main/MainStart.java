55cc5cc00befdd72b367767007032a8aeee986c8
a <1179938183@qq.com>
2021-10-25 14:06:43 +0800

    主启动类中添加程序耗时监控

--- 结对项目/four operations/src/com/main/MainStart.java
+++ 结对项目/four operations/src/com/main/MainStart.java
@@ -2,8 +2,13 @@ package com.main;
 
 import com.controller.InputController;
 
+/**
+ *  主启动类
+ */
 public class MainStart {
     public static void main(String[] args) {
+        long start = System.currentTimeMillis();
         new InputController();
+        System.out.println("耗时: " + (System.currentTimeMillis() - start) + "ms");
     }
 }
